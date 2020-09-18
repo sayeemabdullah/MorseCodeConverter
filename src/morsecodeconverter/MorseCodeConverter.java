d24142bfb67f2bcfa57304cbcb689e69cff02ada
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2020-09-19 05:55:53 +0600

    version 1.0.0 - Translates Text to Morse Code & shows the output with sound

--- src/morsecodeconverter/MorseCodeConverter.java
+++ src/morsecodeconverter/MorseCodeConverter.java
@@ -10,6 +10,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import sun.audio.AudioPlayer;
@@ -21,26 +23,60 @@ import sun.audio.AudioStream;
  */
 public class MorseCodeConverter {
 
-    public static void playSound(String c) throws IOException{
+    public static void playSound(char c) throws IOException{
         InputStream dotSound;
         dotSound = new FileInputStream(new File("sound\\\\morsecode_dot.wav"));
         AudioStream dotAudio = new AudioStream(dotSound);
         InputStream dashSound;
         dashSound = new FileInputStream(new File("sound\\\\morsecode_dash.wav"));
         AudioStream dashAudio = new AudioStream(dashSound);
-        
-        if(".".equals(c)){
+        char dot = '.' ;
+        char dash = '-';
+        if(c==dot){
             AudioPlayer.player.start(dotAudio);
+            System.out.print(dot);
         }
-        else if("-".equals(c)){
+        else if(c==dash){
             AudioPlayer.player.start(dashAudio);
+            System.out.print(dash);
         }
         
     }
     
-    
-    public static void main(String[] args) throws IOException {
-        playSound(".");
+    public static void charToMorse(char ch) throws InterruptedException, IOException{
+            char[] c = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+                  'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                  ',', '.', '?' };
+
+            String[] m = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", 
+                    ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
+                    "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+                    "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
+                    "-----", "--..--", ".-.-.-", "..--.." };
+            
+            for(int i=0;i<c.length;i++){
+                if(ch==c[i]){
+                    String s = m[i];
+                    for(int j=0;j<s.length();j++){
+                        char cha=s.charAt(j);
+                        playSound(cha);
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+                    TimeUnit.SECONDS.sleep(2);
+                    break;
+                }
+            }
     }
     
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input= new Scanner(System.in);
+        String str = input.nextLine();
+        str = str.toLowerCase();
+        for(int i=0;i<str.length();i++){
+            charToMorse(str.charAt(i));
+            System.out.print(" ");
+        }
+    }
 }
